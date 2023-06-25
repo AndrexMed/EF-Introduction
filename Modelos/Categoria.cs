6d97029cb0e95467395eaa29cad8f247d61b11f1
AndrexMed <giovanniandrex@gmail.com>
2023-06-24 22:59:02 -0500

    Metodo GET

--- Modelos/Categoria.cs
+++ Modelos/Categoria.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace IntroEF.Modelos
@@ -17,6 +18,7 @@ namespace IntroEF.Modelos
 
         public string Descripcion { get; set; }
 
+        [JsonIgnore]
         public virtual ICollection<Tarea> Tareas{ get; set; }
 
         public int Peso { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using IntroEF;
+using IntroEF.Migrations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,11 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
     dbContext.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
 
+});
+
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == IntroEF.Modelos.Prioridad.Baja));
 });
 
 app.Run();
